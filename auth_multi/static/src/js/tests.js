e8a25f5fcd3beea4bde71e15749689e6f9a9bc94
Jose Antonio <jose@vauxoo.com>
2016-07-07 11:59:18 -0400

    [ADD] Added first steps to test the login using auth

--- auth_multi/static/src/js/tests.js
+++ auth_multi/static/src/js/tests.js
@@ -6,11 +6,17 @@ odoo.define('auth_multi.tour', function (require) {
     Tour.register({
         id: 'test_instance_oauth_multi',
         name: 'Login Using Google Oauth',
+        path: '/web/login',
         mode: 'test',
         steps: [
             {
                 title: 'Wait for login and redirect',
-                wait: 3000,
+                element: 'input',
+                waitFor: 'input',
+                onload: function(tour){
+                    window.location.href = 'http://127.0.0.1:8069/auth_oauth/signin?state=%7B%22p%22:+3,+%22r%22:+%22http%253A%252F%252Flocalhost:' +
+                    '8069%252Fweb%22,+%22d%22:+%22auth2%22%7D&access_token=ya29.CjXgA59Z3ezBdP_Bki4fth6FtrG9Nw&token_type=Bearer&expires_in=3600'
+                },
             },
             {
                 title: 'Show the main window for the user',
--- auth_multi/tests/test_auth_multi_login.py
+++ auth_multi/tests/test_auth_multi_login.py
@@ -9,13 +9,54 @@
 #    planned by: Nhomar Hernandez <nhomar@vauxoo.com>
 ############################################################################
 
-import openerp.tests
+# import openerp.tests
+from openerp.tests.common import TransactionCase
 import mock
+import requests
+import urllib2
 
 
-@openerp.tests.common.at_install(False)
-@openerp.tests.common.post_install(True)
-class TestOauthMulti(openerp.tests.HttpCase):
+# @openerp.tests.common.at_install(False)
+# @openerp.tests.common.post_install(True)
+# class TestOauthMulti(openerp.tests.HttpCase):
+
+#     def setUp(self):
+#         super(TestOauthMulti, self).setUp()
+
+#     @mock.patch('openerp.addons.auth_oauth.res_users.'
+#                 'res_users._auth_oauth_rpc')
+#     def test_01_login_with_users(self, mock_oauth_rpc):
+#         """Trying the login with google
+#         """
+#         response = {
+#             "id": "123456789987654321",
+#             "email": "test@vauxoo.com",
+#             "verified_email": True,
+#             "name": "Test Oauth",
+#             "given_name": "Test 1",
+#             "family_name": "Oauth",
+#             "gender": "male",
+#             "locale": "es",
+#             "hd": "vauxoo.com"}
+
+#         mock_oauth_rpc.return_value = response
+#         token = 'ya29.CjXgA59Z3ezBdP_Bki4fth6FtrG9Nw'
+#         domain = self.env['ir.config_parameter'].get_param('web.base.url')
+#         path = '/auth_oauth/signin?state=%7B%22p%22:+3,+%22r%22:+%22'\
+#             '{domain}%252Fweb%22,+%22d%22:+%22'\
+#             'auth2%22%7D&access_token={token}&token_type=Bearer'\
+#             '&expires_in=3600'.\
+#             format(domain=domain, token=token)
+#         path = '/web/login'
+#         self.phantom_js(
+#             path,
+#             "odoo.__DEBUG__.services['web.Tour'].run('test_instance_oauth_multi', 'test')",
+#             "odoo.__DEBUG__.services['web.Tour'].tours.test_instance_oauth_multi")
+#         users = self.env['res.users'].search([])
+#         for user in users:
+#             print user.login
+
+class TestOauthMulti(TransactionCase):
 
     def setUp(self):
         super(TestOauthMulti, self).setUp()
@@ -44,7 +85,9 @@ class TestOauthMulti(openerp.tests.HttpCase):
             'auth2%22%7D&access_token={token}&token_type=Bearer'\
             '&expires_in=3600'.\
             format(domain=domain, token=token)
-        self.phantom_js(
-            path,
-            "odoo.__DEBUG__.services['web.Tour'].run('test_instance_oauth_multi', 'test')",
-            "odoo.__DEBUG__.services['web.Tour'].tours.test_instance_oauth_multi")
+        users = self.env['res.users'].search([])
+        print domain + path
+        f = urllib2.urlopen(domain + path)
+        print f.read()
+        for user in users:
+            print user.login
