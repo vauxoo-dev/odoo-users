84f8100c4b5244583938dfe98b4f55324a22dc48
Katherine Zaoral <kathy@vauxoo.com>
2017-11-03 14:40:21 +0000

    [ADD] Unit tests

--- auth_multi/__init__.py
+++ auth_multi/__init__.py
@@ -1,3 +1,4 @@
 from . import controllers
 from . import model
 from . import wizard
+from . import tests
---
+++ auth_multi/tests/test_auth_multi.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from odoo.tests.common import TransactionCase
+
+
+class TestAuthMulti(TransactionCase):
+
+    def setUp(self):
+        """Pseudo-constructor."""
+        super(TestAuthMulti, self).setUp()
+
+    def test_01(self):
+        pass
